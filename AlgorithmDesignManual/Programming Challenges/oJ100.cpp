96e8e6f91264b8b26bd3eabaf4b6007ca293c36e
K. Yasaswi Sri Chandra Gandhi <yasaswisrichandragandhi@gmail.com>
2020-08-07 18:42:50 +0530

    added explanation to 1-1

--- AlgorithmDesignManual/Programming Challenges/oJ100.cpp
+++ AlgorithmDesignManual/Programming Challenges/oJ100.cpp
@@ -8,16 +8,26 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
+    // The OnlineJudge didn't specify number of test cases
+    // so I had to improvise and use this wierd method.
     int a, b;
     while(cin >> a >> b)
     {
+        // creating copies because a > b in some cases.
         int copa = a, copb = b;
+
         long max = 0;
         if (a > b)
             swap (a, b);
+
         for (int i = a; i <= b; i++)
         {
+            // we don't want the value of the loop counter
+            // to change.
             int copy = i;
+
+            // initializing to 1 because the next loop
+            // breaks when the value of the number = 1.
             long ctr = 1;
             while(i != 1)
             {
@@ -27,8 +37,10 @@ int main()
                 else
                     i = 3 * i + 1;
             }
+
             if (ctr > max)
                 max = ctr;
+
             i = copy;
         }
         cout << copa << " " << copb << " " << max << "\n";
